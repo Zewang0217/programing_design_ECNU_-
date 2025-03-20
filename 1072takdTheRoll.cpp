2f93e83cdf60af2fb1ccf7240f1419302c2c6489
Zewang <zewang0217@outlook.com>
2025-03-20 08:28:59 +0800

    1072

---
+++ 1072takdTheRoll.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int CountAbsentStudent(int row, int col) {
+	char ch;
+	int absentCount = 0;
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
+			cin >> ch;
+			if (ch == '.') absentCount++;
+		}
+	}
+	return absentCount;
+}
+
+int main() {
+	int T;
+	cin >> T;
+
+	for (int t = 0; t < T; t++) {
+		int row, col;
+		cin >> row >> col;
+		int res = CountAbsentStudent(row, col);
+		cout << "case #" << t << ":" << endl;
+		cout << res << endl;
+	}
+	return 0;
+}
