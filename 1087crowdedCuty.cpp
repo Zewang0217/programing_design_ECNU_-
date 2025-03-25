7db12a06c78b3f03039deae463fc9a4290bd215b
Zewang <zewang0217@outlook.com>
2025-03-25 23:01:06 +0800

    1087

---
+++ 1087crowdedCuty.cpp
@@ -0,0 +1,22 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+	int C;
+	cin >> C;
+	for (int c = 0; c < C; c++) {
+		int x;
+		cin >> x;
+		int add = 12;
+		int res = x * 4;
+		int h = x;
+		h -= 1;
+		for (; h >= 1; h--) {
+			res += h * add;
+			add += 8;
+		}
+		cout << res << endl;
+	}
+}
+*/
